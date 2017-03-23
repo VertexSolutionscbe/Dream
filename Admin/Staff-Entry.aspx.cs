3ef7fa7d4c06913a515b7f5aac4e0a517450aa62
Linchu John <linchucjohn@gmail.com>
2017-03-23 11:34:42 +0530

    Created Staff Entry Form

---
+++ Admin/Staff-Entry.aspx.cs
@@ -0,0 +1,220 @@
+﻿#region " Using "
+using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Web.Security;
+using Microsoft.Reporting.WebForms;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing;
+#endregion
+
+
+public partial class Admin_Staff_Entry : System.Web.UI.Page
+{
+    int company_id = 0;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+
+            getinvoiceno();
+            show_category();
+            showrating();
+            BindData();
+
+            active();
+            created();
+
+
+
+
+        }
+
+    }
+
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        SqlConnection CON = new SqlConnection(ConfigurationManager.AppSettings["connection"]);
+        SqlCommand cmd = new SqlCommand("insert into Staff_Entry values(@Emp_Code,@Emp_Name,@Emp_Add,@Department,@Branch,@Super_Visor,@Target)", CON);
+        cmd.Parameters.AddWithValue("@Emp_Code", Label1.Text);
+        cmd.Parameters.AddWithValue("@Emp_Name", HttpUtility.HtmlDecode(TextBox3.Text));
+        cmd.Parameters.AddWithValue("@Emp_Add", HttpUtility.HtmlDecode(TextBox2.Text));
+        cmd.Parameters.AddWithValue("@Department", HttpUtility.HtmlDecode(TextBox4.Text));
+        cmd.Parameters.AddWithValue("@Branch", HttpUtility.HtmlDecode(TextBox5.Text));
+        cmd.Parameters.AddWithValue("@Super_Visor", HttpUtility.HtmlDecode(TextBox6.Text));
+        cmd.Parameters.AddWithValue("@Target", HttpUtility.HtmlDecode(TextBox7.Text));
+
+        CON.Open();
+        cmd.ExecuteNonQuery();
+        CON.Close();
+        ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alert Message", "alert('Staff Entry created successfully')", true);
+        BindData();
+        show_category();
+        getinvoiceno();
+        TextBox3.Text = "";
+        TextBox2.Text = "";
+        TextBox4.Text = "";
+        TextBox5.Text = "";
+        TextBox6.Text = "";
+        TextBox7.Text = "";
+
+
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        TextBox3.Text = "";
+        TextBox2.Text = "";
+        TextBox4.Text = "";
+        TextBox5.Text = "";
+        TextBox6.Text = "";
+        TextBox7.Text = "";
+        getinvoiceno();
+        show_category();
+    }
+    private void active()
+    {
+
+    }
+    protected void lnkView_Click(object sender, EventArgs e)
+    {
+        GridViewRow grdrow = (GridViewRow)((LinkButton)sender).NamingContainer;
+
+
+        LinkButton Lnk = (LinkButton)sender;
+        string name = Lnk.Text;
+        Session["name"] = name;
+        Response.Redirect("Account_show.aspx");
+
+
+    }
+
+    private void created()
+    {
+
+    }
+    protected void BindData()
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.AppSettings["connection"]);
+        SqlCommand CMD = new SqlCommand("select * from Staff_Entry ORDER BY Emp_Code asc", con);
+        DataTable dt1 = new DataTable();
+        SqlDataAdapter da1 = new SqlDataAdapter(CMD);
+        da1.Fill(dt1);
+        GridView1.DataSource = dt1;
+        GridView1.DataBind();
+
+    }
+    protected void ImageButton9_Click(object sender, ImageClickEventArgs e)
+    {
+        ImageButton img = (ImageButton)sender;
+        GridViewRow row = (GridViewRow)img.NamingContainer;
+        SqlConnection con = new SqlConnection(ConfigurationManager.AppSettings["connection"]);
+        SqlCommand cmd = new SqlCommand("delete from Staff_Entry where Emp_Code='" + row.Cells[0].Text + "' ", con);
+        con.Open();
+        cmd.ExecuteNonQuery();
+        con.Close();
+        ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alert Message", "alert('Staff Details deleted successfully')", true);
+
+        BindData();
+        show_category();
+        getinvoiceno();
+
+
+    }
+    private void getinvoiceno()
+    {
+        int a;
+
+        SqlConnection con1 = new SqlConnection(ConfigurationManager.AppSettings["connection"]);
+        con1.Open();
+        string query = "Select Max(Emp_Code) from Staff_Entry";
+        SqlCommand cmd1 = new SqlCommand(query, con1);
+        SqlDataReader dr = cmd1.ExecuteReader();
+        if (dr.Read())
+        {
+            string val = dr[0].ToString();
+            if (val == "")
+            {
+                Label1.Text = "1";
+            }
+            else
+            {
+                a = Convert.ToInt32(dr[0].ToString());
+                a = a + 1;
+                Label1.Text = a.ToString();
+            }
+        }
+    }
+    private void show_category()
+    {
+
+        SqlConnection con = new SqlConnection(ConfigurationManager.AppSettings["connection"]);
+        SqlCommand cmd = new SqlCommand("Select * from Staff_Entry ORDER BY Emp_Code asc", con);
+        con.Open();
+        DataSet ds = new DataSet();
+        SqlDataAdapter da = new SqlDataAdapter(cmd);
+        da.Fill(ds);
+
+
+        DropDownList2.DataSource = ds;
+        DropDownList2.DataTextField = "Emp_Name";
+        DropDownList2.DataValueField = "Emp_Code";
+        DropDownList2.DataBind();
+        DropDownList2.Items.Insert(0, new ListItem("All", "0"));
+
+        con.Close();
+    }
+    protected void LoginLink_OnClick(object sender, EventArgs e)
+    {
+        FormsAuthentication.SignOut();
+        Response.Redirect("~/login.aspx");
+
+    }
+
+    protected void btnRandom_Click(object sender, EventArgs e)
+    {
+        Session["name1"] = "";
+        Response.Redirect("~/Admin/Category_Add.aspx");
+    }
+
+    private void showcustomertype()
+    {
+
+    }
+    private void showrating()
+    {
+
+    }
+    protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        GridView1.PageIndex = e.NewPageIndex;
+        BindData();
+    }
+    protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+        if (e.Row.RowType == DataControlRowType.Footer)
+        {
+            e.Row.Cells[0].Text = "Page " + (GridView1.PageIndex + 1) + " of " + GridView1.PageCount;
+        }
+    }
+    protected void LinkButton1_Click(object sender, EventArgs e)
+    {
+
+    }
+    protected void TextBox1_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+    protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+}
